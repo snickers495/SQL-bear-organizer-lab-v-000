ec5e5f63ede379ae6f96aeeabb4f646874b9f6e9
Lisa Lee <snickers495@gmail.com>
2017-10-17 16:50:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears ORDER BY DESC LIMIT 1;"
+  "SELECT color, COUNT(color) FROM bears ORDER BY DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE name = null;"
 end
